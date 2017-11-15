76a039caee824e1343c142d4c96eea7eaef1c27f
vinh <12>
2017-11-15 07:03:27 +0700

    Initial commit

---
+++ app/src/main/java/com/test1/vo/myapplication/Model/Vehicle.java
@@ -0,0 +1,28 @@
+package com.test1.vo.myapplication.Model;
+
+import javax.inject.Inject;
+
+/**
+ * Created by vinh on 11/15/2017.
+ */
+
+public class Vehicle {
+    private Motor motor;
+
+    @Inject
+    public Vehicle(Motor motor){
+        this.motor = motor;
+    }
+
+    public void stop(){
+        motor.breakMotor();
+    }
+
+    public void increaseSpeed(int value){
+        motor.accelerate(value);
+    }
+
+    public int getSpeed(){
+        return motor.getRpm();
+    }
+}
